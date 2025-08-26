757f68d5500b2f3564bc78bc678c11f6b32de710
Leo <leooproencaaa@gmail.com>
2025-08-26 17:51:04 -0300

    Grouping Operator

--- scripts.js
+++ scripts.js
@@ -1,24 +1,8 @@
-let number = 10
+let total1 = 2 + 3 * 4
+console.log(total1)
 
+let total2 = (2 + 3) * 4
+console.log(total2)
 
-// Incremento
-// number = number + 1
-number++
-
-// Incrementa após por isso não mostra no console
-console.log(number++)
-console.log(number)
-
-// Incrementa antes
-console.log(++number)
-
-// Decremento
-// console.log("Decremento após: ", number--)
-// console.log(number--)
-console.log("Decremento antes: ", --number)
-
-number += 10
-console.log(number)
-
-number -= 2
-console.log(number)
+let average = (9.5 + 7 + 5) / 3
+console.log(average)    
