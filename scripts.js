a7491d11c13a67c560412091298651859b15711c
Leo <leooproencaaa@gmail.com>
2025-08-26 17:31:46 -0300

    Operadores Aritméticos

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!") // Exibe uma mensagem no console do navegador
+// Operadores Aritméticos
+
+console.log("SOMA: ", 12 + 8)
+console.log("CONCATENAÇÂO: ", "12" + "8")
+
+console.log("SUBTRAÇÂO:", 12 - 10)
+
+console.log("MULTIPLICAÇÂO:", 12 * 8)
+
+console.log("DIVISÂO:", 12 / 2)
+
+console.log("RESTO DA DIVISÂO:", 13 % 2)
+
+console.log("POTENCIAÇÃO:", 2 ** 3)
