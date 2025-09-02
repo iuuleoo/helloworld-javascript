32a91af5ffecb8a818ae677d180c40359be6ce7b
Leo <leooproencaaa@gmail.com>
2025-09-02 16:19:17 -0300

    Argumentos e Parâmetros

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
-function message() {
-  console.log("Olá. É bom ter você aqui!")
+function message(username) {
+  console.log("Olá", username)
+  
 }
 
-message()
-message()
-message()
+message("Oi")
+message("Eai")
+
+function sum(a, b) {
+  console.log(a + b)
+}
+
+sum(10,20)
+sum(7,3)
+
+function joinText(text1, text, text2){
+  console.log(text1, text2, text3)
+}
+
+joinText("Oi", "eai", "Tudo bem")
+joinText("eai", "Oi", "Tudo bem")
+
+joinText("Oi", "eai", "")
